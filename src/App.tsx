8db883e0512babc2a1489feebdfa753919b0be37
Mohan Muthukumar <mohan@extrasalt.org>
2025-08-14 18:37:18 +0530

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Resume from "./Resume";
+import logo from "../assets/diamondsvg.svg";
 
 export function App() {
   return (
     <div>
       <nav className="bg-background text-foreground p-4">
         <div className="container mx-auto flex justify-between">
-          <Link to="/" className="text-lg font-bold">My Portfolio</Link>
+          <img src={logo} alt="logo" className="w-10 h-10" />
           <div>
             <Link to="/" className="mr-4">Home</Link>
             <Link to="/resume">Resume</Link>
